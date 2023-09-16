cd57142e8f419dafd3a6ba96aae5d2de2acf5e4a
PCLakshan <p.cha.lakshan@gmail.com>
2023-09-16 20:51:54 +0530

    first commit

---
+++ src/supermarket/Supermarket.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Supermarket;
+package item;
+import item.view.ItemView;
+import Customer.view.CustomerView;
+
+/**
+ *
+ * @author A-Z Electronic
+ */
+public class SuperMarket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   //new ItemView().setVisible(true);
+     new CustomerView().setVisible(true);
+    }
+    
+}
